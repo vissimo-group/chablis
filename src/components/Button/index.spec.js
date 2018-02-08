1dd0614e7e6ccabb76ebda70e706c9a7d37a2c9d
Henrique Sosa <henriquesosa@gmail.com>
2018-02-08 07:44:47 -0200

    Update tests

--- src/components/Button/index.spec.js
+++ src/components/Button/index.spec.js
@@ -1,5 +1,27 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Button from './';
+
+const onClick = jest.fn();
+const component = mount(<Button onClick={onClick}>Button</Button>);
+
+const click = () => {
+  component.find('button').simulate('click');
+};
+
 describe('Button', () => {
-  it('Should return true for tests', () => {
-    expect(true).toBeTruthy();
+  it('Should render a button', () => {
+    expect(component.find('button')).toBeTruthy();
+  });
+
+  it('Should allow client`s click', () => {
+    click();
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should not allow client`s click', () => {
+    component.setProps({ disabled: true });
+    click();
+    expect(onClick).toHaveBeenCalledTimes(1);
   });
 });
