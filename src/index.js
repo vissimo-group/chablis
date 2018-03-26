ebddbf38b6e58b4d01cd53cd46880cb2e2bdc388
Henrique Sosa <henriquesosa@gmail.com>
2018-03-26 13:15:27 -0300

    export Title component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import Button from './components/Button';
 import Tooltip from './components/Tooltip';
 import { Container, Column, Row } from './components/Grid';
+import { Title } from './components/Typography';
 
 export default {
   Button,
   Container,
   Column,
   Row,
+  Title,
   Tooltip,
 };
