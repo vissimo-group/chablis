399c34d6b23862539d5cce89ee3dc3bbd6e71154
Antonio Junior <junyor@hotmail.co.uk>
2021-02-22 13:53:48 -0300

    TEC-1284: Setting variables on CustomTooltipWrapper.

--- src/components/CustomTooltip/CustomTooltipWrapper.js
+++ src/components/CustomTooltip/CustomTooltipWrapper.js
@@ -2,25 +2,33 @@ import styled from 'styled-components';
 
 import TooltipWrapper from '../Tooltip/TooltipWrapper';
 
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  sizes,
+} from '../../styles/variables';
+
 const CustomTooltipWrapper = styled(TooltipWrapper)`
   position: absolute;
   min-width: 255px;
   max-width: 280px;
-  padding: 16px 24px;
+  padding: ${sizes.DEFAULT} 24px;
   ${({ alignY }) => alignY}: calc(100% + 15px);
   ${({ alignX }) => alignX}: ${({ spaced }) => (spaced ? '-55px' : 'calc(50% - 32px)')};
 
   img {
     width: 100%;
     height: auto;
-    margin-bottom: 16px;
+    margin-bottom: ${sizes.DEFAULT};
     pointer-events: none;
   }
 
   p {
-    color: #696969;
-    font-size: 0.75rem;
-    line-height: 0.875rem;
+    color: ${colors.DARKEST_GREY};
+    font-size: ${fontSizes.EXTRASMALL};
+    font-weight: ${fontWeights.REGULAR};
+    line-height: ${fontSizes.SMALL};
     text-align: left;
   }
 `;
