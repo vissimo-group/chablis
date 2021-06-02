301acd83ce85e10bbb808b7096d25ad102d4b254
Antonio Junior <junyor@hotmail.co.uk>
2021-06-01 23:24:34 -0300

    TEC-1651: Creating price component.

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -1,4 +1,4 @@
-export const colors = {
+const colors = {
   PRIMARY: '#03A678',
   PRIMARY_DARKEN: '#0D7557',
   SECONDARY: '#CE2A36',
@@ -6,13 +6,15 @@ export const colors = {
   LIGHTEST_GREY: '#DCDCDC',
   LIGHT_GREY: '#D3D3D3',
   MEDIUM_GREY: '#C0C0C0',
+  MEDIUM_GREY_2: '#7a7b7f',
   DARK_GREY: '#808080',
   DARKEST_GREY: '#696969',
   DARKEST_GREY_2: '#393939',
   BLACK: '#20201E',
+  DEFAULT_BLACK: '#000',
 };
 
-export const sizes = {
+const sizes = {
   DEFAULT: '16px',
   LARGE: '32px',
   HALF: '8px',
@@ -20,14 +22,14 @@ export const sizes = {
   FULL: '100%',
 };
 
-export const breakpoints = {
+const breakpoints = {
   SMALL: '768px',
   NORMAL: '1024px',
   BIG: '1280px',
   LARGE: '1440px',
 };
 
-export const fontSizes = {
+const fontSizes = {
   HUGE1: '48px',
   HUGE2: '36px',
   LARGE: '32px',
@@ -38,8 +40,23 @@ export const fontSizes = {
   TINY: '10px',
 };
 
-export const fontWeights = {
+const fontWeights = {
   LIGHT: '300',
   REGULAR: '400',
+  MEDIUM: '500',
+  SEMIBOLD: '600',
   BOLD: '700',
+};
+
+const fonts = {
+  DEFAULT: 'Montserrat',
+};
+
+export {
+  breakpoints,
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  sizes,
 };
