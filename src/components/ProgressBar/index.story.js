08c23078378a671b930888e2772bf2836845b574
Caio <caioag42@gmail.com>
2021-06-08 11:05:31 -0300

    label width (#42)
    Co-authored-by: Caio Augusto <caio.augusto@evino.com.br>

--- src/components/ProgressBar/index.story.js
+++ src/components/ProgressBar/index.story.js
@@ -9,4 +9,12 @@ storiesOf('ProgressBar', module)
     <ProgressBar active={1} label="Açúcar" />
   )).addWithInfo('ProgressBar with no label', () => (
     <ProgressBar active={1} />
+  ))
+  .addWithInfo('ProgressBar with labelWidth', () => (
+    <div>
+      <div style={{ marginBottom: 10 }}>
+        <ProgressBar active={1} label="Açúcar" labelWidth="130px" />
+      </div>
+      <ProgressBar active={3} label="Monossacarídeo" labelWidth="130px" />
+    </div>
   ));
